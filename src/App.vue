8f16d27fcad06dea38476874e9578e51cc588fe1
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-26 09:10:46 +0100

    Début

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/
+dist/css/bootstrap.min.css" integrity="sha384-
+rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
